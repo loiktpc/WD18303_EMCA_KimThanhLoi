37f0b35cdd0145a54aace19c51beb8183dfb49f7
loikt <loiktpc05314@fpt.edu.vn>
2024-02-03 09:51:11 +0700

    lab7

---
+++ lab7/index.js
@@ -0,0 +1,8 @@
+const names = "Kim Thanh Loi";
+								const person = {names};
+								console.log(person);
+								const sayHelloLinting = (fName) => {
+									console.log(`Hello Linting, ${fName}`);
+								};
+
+sayHelloLinting('Kim Thanh Loi');
